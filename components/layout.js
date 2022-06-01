0a2b5da73630aaba6650e077a9b15f2a6817b185
Matthew Jett <matthew.jett@willbrook.com>
2022-06-01 12:55:21 -0600

    new commit

--- components/layout.js
+++ components/layout.js
@@ -1,48 +1,79 @@
-import styles from './layout.module.css'
-import Image from 'next/image'
-import Head from 'next/head'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+/* /components/Layout.js */
 
+import React, { useContext } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import { Container, Nav, NavItem } from "reactstrap";
+import AppContext from "./context";
 
-const name = "Matthew"
-export const siteTitle = "Next.js Sample Website"
-export default function Layout({children, isHome}) {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <meta name="description" content="Learn how to build a personal website using Next.js" />
-            <header className={styles.header}>
-                {isHome? (
-                    <>
-                        <Image priority src="/images/biden3.jpeg" className={utilStyles.borderCircle} height={144} width={144} alt={name} />
-                    </>
+const Layout = (props) => {
+const title = "Welcome to Nextjs";
+const {user} = useContext(AppContext);
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossOrigin="anonymous"
+        />
+        <script src="https://js.stripe.com/v3" />
+      </Head>
+      <header>
+        <style jsx>
+          {`
+            a {
+              color: white;
+            }
+            h5 {
+              color: white;
+              padding-top: 11px;
+            }
+          `}
+        </style>
+        <Nav className="navbar navbar-dark bg-dark">
+          <NavItem>
+            <Link href="/">
+              <a className="navbar-brand">Home</a>
+            </Link>
+          </NavItem>
+          <NavItem className="ml-auto">
+            {user ? (
+              <h5>{user.username}</h5>
+            ) : (
+              <Link href="/register">
+                <a className="nav-link"> Sign up</a>
+              </Link>
+            )}
+          </NavItem>
+          <NavItem>
+            {user ? (
+              <Link href="/">
+                <a
+                  className="nav-link"
+                  onClick={() => {
+                    logout();
+                    setUser(null);
+                  }}
+                >
+                  Logout
+                </a>
+              </Link>
+            ) : (
+              <Link href="/login">
+                <a className="nav-link">Sign in</a>
+              </Link>
+            )}
+          </NavItem>
+        </Nav>
+      </header>
+      <Container>{props.children}</Container>
+    </div>
+  );
+};
 
-                ) : (
-                    <>
-                        <Image priority src="/images/biden3.jpeg" className={utilStyles.borderCircle} height={108} width={108} alt={name} />
-                        <h2 className={utilStyles.headingLg}>
-                            <Link href="/">
-                                <a className={utilStyles.colorInherit}>{name}</a>
-                            </Link>
-                        </h2>
-                    </>
-
-
-                )}
-            </header>
-            <main>{children}</main>
-            {/* {!isHome && (
-                <div className={styles.backToHome}>
-                    <Link href="/">
-                        <a>Back To Home</a>
-                    </Link>    
-                </div>
-            )} */}
-            
-        </div>
-       
-        )
-}
+export default Layout;
