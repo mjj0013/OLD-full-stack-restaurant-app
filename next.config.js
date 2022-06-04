fdeb521a914a1587203c3bff4d1a1152260d5958
Matthew Jett <mjj0013@uah.edu>
2022-06-04 15:55:42 -0500

    new commit

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.css$/,
-      loader: "style-loader!css-loader"
-    }, {
-      test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-      loader: 'url-loader?limit=100000' }
-    )
-    return config
-  },
+  // webpack: (config, options) => {
+  //   config.module.rules.push({
+  //     test: /.jsx?$/,
+  //     loader: 'babel-loader',
+  //     exclude: /node_modules/
+  //   }, {
+  //     test: /\.css$/,
+  //     loader: "style-loader!css-loader"
+  //   }, {
+  //     test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+  //     loader: 'url-loader?limit=100000' }
+  //   )
+  //   return config
+  // },
 }
 
 module.exports = nextConfig
